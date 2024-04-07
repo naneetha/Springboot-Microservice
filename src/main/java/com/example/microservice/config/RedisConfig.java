996688bde0f9f30adad67904611987ab83de2705
Aneetha <115028836+naneetha@users.noreply.github.com>
2024-04-07 10:45:16 -0700

    Add files via upload

---
+++ src/main/java/com/example/microservice/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.example.microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+
+}
